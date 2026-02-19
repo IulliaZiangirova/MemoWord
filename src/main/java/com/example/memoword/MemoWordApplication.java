db75fef117fec49c6cf3043bd4952068081141bc
Julia Ziangirova <juliaziangirova@MacBook-Air-Julia.local>
2026-02-18 18:00:57 -0800

    first commit

---
+++ src/main/java/com/example/memoword/MemoWordApplication.java
@@ -0,0 +1,13 @@
+package com.example.memoword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoWordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoWordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/memoword/MemoWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memoword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoWordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
